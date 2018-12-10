3d49767a7a37cae9a7ce6d7600fc40c72ea5dadf
htanivar <37423501+htanivar@users.noreply.github.com>
2018-12-10 20:41:08 +0000

    Initial Commit for Spring Boot Application

---
+++ src/main/java/com/qa/challenge/ravijaganathan/RaviJaganathanApplication.java
@@ -0,0 +1,12 @@
+package com.qa.challenge.ravijaganathan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaviJaganathanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaviJaganathanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/qa/challenge/ravijaganathan/RaviJaganathanApplicationTests.java
@@ -0,0 +1,19 @@
+package com.qa.challenge.ravijaganathan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RaviJaganathanApplicationTests {
+
+	String[] args = {};
+
+	@Test
+	public void contextLoads() {
+		RaviJaganathanApplication.main( args);
+	}
+
+}

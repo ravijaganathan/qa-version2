8a9b7cec9a327070c1e23d7d08116aecd7097651
htanivar <37423501+htanivar@users.noreply.github.com>
2018-12-11 23:28:03 +0000

    Added: Test Create Account is deployed

--- src/main/java/com/qa/challenge/ravijaganathan/controller/AccountRestController.java
+++ src/main/java/com/qa/challenge/ravijaganathan/controller/AccountRestController.java
@@ -3,10 +3,11 @@ package com.qa.challenge.ravijaganathan.controller;
 import com.qa.challenge.ravijaganathan.model.entitiy.Account;
 import com.qa.challenge.ravijaganathan.model.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class AccountRestController {
@@ -15,21 +16,35 @@ public class AccountRestController {
     AccountService accountService;
 
 
-    @RequestMapping(method= RequestMethod.POST,value="/account-project/rest/account/json")
-    public void addSecurity(@RequestBody Account account) {
-        accountService.addAccount(account);
+    @PostMapping("/account-project/rest/account/json")
+    ResponseMessage newEmployee(@RequestBody Account account) {
+        Account createdAccount = accountService.addAccount(account);
+        ResponseMessage responseMessage = new ResponseMessage();
+        if(Optional.ofNullable(createdAccount).isPresent()) {
+            responseMessage.setMessage("Successfully Added");
+        }
+
+        return responseMessage;
     }
 
 
 
+    class ResponseMessage{
+        String message;
 
-}
+        public String getMessage() {
+            return message;
+        }
 
-class ResponseMessage{
-    String message;
+        public ResponseMessage() {}
+        public ResponseMessage(String message) {
+            this.message = message;
+        }
 
-    public ResponseMessage(String message) {
-        this.message = message;
+        public void setMessage(String message) {
+            this.message = message;
+        }
     }
 }
+
 

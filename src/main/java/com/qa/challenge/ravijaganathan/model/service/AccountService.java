effbcf883ca40dd628af5de4f236280bcb633090
htanivar <37423501+htanivar@users.noreply.github.com>
2018-12-11 14:33:58 +0000

    Account Service is created

---
+++ src/main/java/com/qa/challenge/ravijaganathan/model/service/AccountService.java
@@ -0,0 +1,27 @@
+package com.qa.challenge.ravijaganathan.model.service;
+
+import com.qa.challenge.ravijaganathan.model.entitiy.Account;
+import com.qa.challenge.ravijaganathan.model.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class AccountService {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    public List<Account> getAllAccounts(){
+
+        List<Account> returnList = new ArrayList<>();
+        accountRepository.findAll().forEach(returnList::add);
+        return returnList;
+    }
+
+
+}

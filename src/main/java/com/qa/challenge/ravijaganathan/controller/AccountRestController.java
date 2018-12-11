37335093001b373e6f6b9a788990acebc44eda2f
htanivar <37423501+htanivar@users.noreply.github.com>
2018-12-11 23:49:49 +0000

    Added: Delete Account through API Endpoint

--- src/main/java/com/qa/challenge/ravijaganathan/controller/AccountRestController.java
+++ src/main/java/com/qa/challenge/ravijaganathan/controller/AccountRestController.java
@@ -3,9 +3,7 @@ package com.qa.challenge.ravijaganathan.controller;
 import com.qa.challenge.ravijaganathan.model.entitiy.Account;
 import com.qa.challenge.ravijaganathan.model.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -21,10 +19,16 @@ public class AccountRestController {
         Account createdAccount = accountService.addAccount(account);
         ResponseMessage responseMessage = new ResponseMessage();
         if(Optional.ofNullable(createdAccount).isPresent()) {
-            responseMessage.setMessage("Successfully Added");
+            responseMessage.setMessage("account has been successfully added");
         }
 
         return responseMessage;
+    }
+
+
+    @RequestMapping(method= RequestMethod.DELETE,value="/account-project/rest/account/json/{id}")
+    public void deleteSecurity(@PathVariable Long id) {
+        accountService.deleteAccount(id);
     }
 
 

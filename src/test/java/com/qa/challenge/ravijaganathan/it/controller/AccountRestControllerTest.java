8559ad3fd73e012e3d86af893f3dd1cc6a6bad2d
htanivar <37423501+htanivar@users.noreply.github.com>
2018-12-12 01:42:00 +0000

    Added: @Before Annotation to clear the test

--- src/test/java/com/qa/challenge/ravijaganathan/it/controller/AccountRestControllerTest.java
+++ src/test/java/com/qa/challenge/ravijaganathan/it/controller/AccountRestControllerTest.java
@@ -2,6 +2,7 @@ package com.qa.challenge.ravijaganathan.it.controller;
 
 import com.qa.challenge.ravijaganathan.model.entitiy.Account;
 import com.qa.challenge.ravijaganathan.model.service.AccountService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountRestControllerTest {
     @Autowired
     AccountService accountService;
+
+
+    
 
     @Test
     public void createAccount_test(){
@@ -47,8 +51,18 @@ public class AccountRestControllerTest {
 
     @Test
     public void findAllAccounts_Test() throws NoSuchMethodException {
-        createAccount_test();
         assertThat(accountService.getAllAccounts()).hasSize(3);
+    }
+
+
+    @Test
+    public void addAdditional() throws NoSuchMethodException {
+        Account fourthAccount = new Account();
+        fourthAccount.setFirstName("Steven");
+        fourthAccount.setLastName("Doe");
+        fourthAccount.setAccountNumber("1238");
+        accountService.addAccount(fourthAccount);
+        assertThat(accountService.getAllAccounts()).hasSize(4);
 
     }
 
